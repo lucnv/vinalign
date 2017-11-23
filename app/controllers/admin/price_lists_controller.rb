83c3ce4ebff0814d7b764610f4533468a600557d
lucnv <nguyenluc312@gmail.com>
2017-11-24 04:07:00 +0700

    admin-edit-price-list

--- app/controllers/admin/price_lists_controller.rb
+++ app/controllers/admin/price_lists_controller.rb
@@ -1,8 +1,9 @@
 class Admin::PriceListsController < Admin::BaseController 
-  before_action :patient_record
+  before_action :patient_record, only: [:index, :new, :create]
+  before_action :price_list, only: [:edit, :update]
 
   def index
-    price_lists
+    price_list_collection
   end
 
   def new 
@@ -12,7 +13,21 @@ class Admin::PriceListsController < Admin::BaseController
   def create
     @price_list = @patient_record.price_lists.build price_list_params
     if @price_list.save
-      price_lists
+      price_list_collection
+      flash.now[:success] = t ".success"
+    else
+      flash.now[:failed] = t ".failed"
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @price_list.assign_attributes price_list_params
+    if @price_list.save 
+      @patient_record = @price_list.patient_record
+      price_list_collection
       flash.now[:success] = t ".success"
     else
       flash.now[:failed] = t ".failed"
@@ -24,11 +39,15 @@ class Admin::PriceListsController < Admin::BaseController
     @patient_record = PatientRecord.find params[:patient_record_id]
   end
 
+  def price_list 
+    @price_list = PriceList.find params[:id]
+  end
+
   def price_list_params
     params.require(:price_list).permit PriceList::ATTRIBUTES
   end
 
-  def price_lists
+  def price_list_collection
     @price_lists = @patient_record.price_lists
   end
 end
