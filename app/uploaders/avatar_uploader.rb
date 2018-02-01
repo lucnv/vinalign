55c8e964a73cc4b3eab2a4a927473ff470de0f2c
oDoHongSon <do.hong.son@framgia.com>
2018-02-01 17:38:08 +0700

    config s3

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -5,8 +5,8 @@ class AvatarUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
+  # storage :file
+  storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
@@ -31,7 +31,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
     process resize_to_fill: Settings.avatar.versions.medium
   end
 
-  version :small do 
+  version :small do
     process resize_to_fill: Settings.avatar.versions.small
   end
 
