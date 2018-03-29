027b87572223dee34db45b16df66dcce932d6baf
nguyenluc312 <nguyenluc312@gmail.com>
2018-03-30 02:28:00 +0700

    support-search-clinics-vietnamese

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180328210901) do
+ActiveRecord::Schema.define(version: 20180329185209) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 20180328210901) do
     t.string "facebook"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "normalized_name"
     t.index ["district_id"], name: "index_clinics_on_district_id"
   end
 
