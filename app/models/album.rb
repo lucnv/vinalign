bebc8daea4f8c50a3eb2202f46b0254073895e04
oDoHongSon <do.hong.son@framgia.com>
2018-04-02 17:44:32 +0700

    fix admin album

--- app/models/album.rb
+++ app/models/album.rb
@@ -5,5 +5,7 @@ class Album < ApplicationRecord
   has_many :images, dependent: :destroy
   has_many :notifications, as: :notifiable, dependent: :destroy
 
+  scope :recent_updated, ->{order updated_at: :desc}
+
   validates :name, presence: true, uniqueness: {scope: :treatment_phase_id}
 end
