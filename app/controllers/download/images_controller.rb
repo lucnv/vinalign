3eb13fc442a5d7a5d508a0ac6f0d23ed95b8c7fe
oDoHongSon <do.hong.son@framgia.com>
2018-04-05 21:22:03 +0700

    fix css delete images

--- app/controllers/download/images_controller.rb
+++ app/controllers/download/images_controller.rb
@@ -3,6 +3,6 @@ class Download::ImagesController < ApplicationController
     image = Image.find params[:id]
     file_name = File.basename image.source.path
     data = open(image.source.url)
-    send_data data.read, :type => data.content_type, :x_sendfile => true
+    send_data data.read, filename: file_name, type: data.content_type, x_sendfile: true
   end
 end
--- app/controllers/download/treatment_plan_files_controller.rb
+++ app/controllers/download/treatment_plan_files_controller.rb
@@ -3,6 +3,6 @@ class Download::TreatmentPlanFilesController < ApplicationController
     treatment_plan_file = TreatmentPlanFile.find params[:id]
     file_name = File.basename treatment_plan_file.source.path
     data = open(treatment_plan_file.source.url)
-    send_data data.read, :type => data.content_type, :x_sendfile => true
+    send_data data.read, filename: file_name, type: data.content_type, x_sendfile: true
   end
 end
